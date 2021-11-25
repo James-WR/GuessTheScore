c96e094ecdcd7e4b349a3cfe05b7588849719ead
James Robbs <90396565+James-WR@users.noreply.github.com>
2021-11-25 10:47:19 +0000

    Update member.rb

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,6 @@
 class Member < ApplicationRecord
   belongs_to :community
+  belongs_to :user
   has_many :member_guesses
   validates :overall_exact, :overall_fuzzy,
             :overall_points, presence: true, numericality: { only_integer: true }
