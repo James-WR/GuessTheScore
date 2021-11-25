eb87548b6920907e4799f1783788e05f8fdc6e59
alicia borchert <16aliciab@gmail.com>
2021-11-25 16:23:36 +0000

    fixed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :communities do
-    resources :member_guesses, only: [:new, :create, :update]
-    resources :members, only: [:new, :create, :update]
+    resources :members, only: [:new, :create, :update] do
+      resources :member_guesses, only: [:new, :create, :update]
+    end
   end
 end
