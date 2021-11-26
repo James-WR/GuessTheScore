2a84a20b9e3510d55436c8ab1d87204b1562cb3d
alicia borchert <16aliciab@gmail.com>
2021-11-26 15:46:21 +0000

    changed name to Jonathan for first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user = User.create(
-  first_name: "John",
+  first_name: "Jonathan",
   last_name: "Smith",
   username: "jsmith",
   email: "jsmith@gmail.com",
