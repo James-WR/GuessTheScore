09ac4c4c5f100ec4c46c026e420819309bafdee4
Christopher Hampson <champson@tutanota.com>
2021-11-29 11:34:29 +0000

    Add owner as member on creation of community

--- app/models/community.rb
+++ app/models/community.rb
@@ -3,4 +3,10 @@ class Community < ApplicationRecord
   belongs_to :league
   has_many :members
   validates :community_name, presence: true
+
+  after_create :create_member
+
+  def create_member
+    Member.create(user: owner, community: self, overall_exact: 0, overall_fuzzy: 0, overall_points: 0)
+  end
 end
