b713d6849507fc845eb53a8a8439e67be6b6a005
Christopher Hampson <champson@tutanota.com>
2021-11-29 12:12:51 +0000

    Fix display of other leagues and match_days

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -3,11 +3,21 @@ class CommunitiesController < ApplicationController
     @communities = Community.all
   end
 
+  def find_match_day_number(league_name)
+    if league_name == "Premier League"
+      match_day_num = 15
+    elsif league_name == "Women's Super League"
+      match_day_num = 9
+    elsif league_name == "Bundesliga"
+      match_day_num = 14
+    end
+  end
+
   def show
-    @member_guesses = current_user.member_guesses.order(created_at: :asc).select { |m| m.fixture.match_day == 13 }
     @community = Community.find(params[:id])
+    @member_guesses = current_user.member_guesses.order(created_at: :asc).select { |m| m.fixture.match_day == find_match_day_number(@community.league.league_name) && m.fixture.league == @community.league}
     @sorted_members = @community.members.order(overall_points: :desc, overall_exact: :desc, overall_fuzzy: :desc)
-    Fixture.where(league_id: 1, match_day: 12).each do |fixture|
+    Fixture.where(league_id: 3, match_day: 10).each do |fixture|
       @community.members.each do |member|
         member.update(weekly_exact: 0, weekly_fuzzy: 0, weekly_points: 0)
         weekly_exact = 0
@@ -36,6 +46,7 @@ class CommunitiesController < ApplicationController
   def create
     @community = Community.new(community_params)
     @community.owner = current_user
+
     if @community.save
       redirect_to community_path(@community)
     else
