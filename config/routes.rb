1d6eeacfce8f969cf9117c4f06019731ae075eb6
Christopher Hampson <champson@tutanota.com>
2021-12-01 09:06:19 +0000

    Added custom 404 and 500 rendering

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_server'
+  get '/422', to: 'errors#unprocessable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :communities do
     collection do
